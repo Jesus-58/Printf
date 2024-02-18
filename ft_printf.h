cacbd49acffe93b048da9cdbfd71c3ef1e8edef0
Jesus-58 <140073012+Jesus-58@users.noreply.github.com>
2024-02-18 21:27:19 +0100

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -14,6 +14,16 @@
 # define PRINTF_H
 
 #include <stdarg.h>
+#include <unistd.h>
 
+int			ft_putchar(int c);
+int			ft_putstr(char *s);
+int			ft_putnbr(int n);
+int			ft_putunbr(unsigned int n);
+int			ft_puthex_minus(unsigned int n);
+int			ft_puthex_mayus(unsigned int n);
+int			ft_putptr(unsigned long n);
+static int	select_format(char type, va_list args);
+int			ft_printf(char const *format, ...);
 
 #endif
